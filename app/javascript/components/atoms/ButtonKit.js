5423f1b005e8f7dfaf1a60450f66f018b63934d8
hamasaki <mfhama9910@icloud.com>
2021-05-27 05:10:58 +0900

    support for responsive design

--- app/javascript/components/atoms/ButtonKit.js
+++ app/javascript/components/atoms/ButtonKit.js
@@ -2,46 +2,55 @@ import styled from 'styled-components'
 
 export const PrimaryButton = styled.button`
   font-size: 1rem;
-  margin: 0 0.5rem;
+  margin: 0.125rem 0.5rem;
   padding: 0.15rem 1rem;
   border-radius: 3px;
   color: #fff;
   background: rgba(103,58,183,0.5);
   border: 1px solid rgba(103,58,183,0.5);
-  &:hover {
-    background: rgba(103,58,183,0.6);
-  }`
+  @media (min-width: 576px) {
+    &:hover {
+      background: rgba(103,58,183,0.6);
+    }
+  }
+`
 
 export const SecondaryButton = styled.button`
   font-size: 1rem;
-  margin: 0 0.5rem;
+  margin: 0.125rem 0.5rem;
   padding: 0.15rem 1rem;
   border-radius: 3px;
   color: rgba(103,58,183,0.5);
   background: #fff;
   border: 1px solid rgba(103,58,183,0.5);
-  &:hover {
-    background: rgba(103,58,183,0.1);
+  @media (min-width: 576px) {
+    &:hover {
+      background: rgba(103,58,183,0.1);
+    }
   }
 ` 
 
 export const GreyButton = styled.button`
   font-size: 1rem;
-  margin: 0 0.5rem;
+  margin: 0.125rem 0.5rem;
   padding: 0.15rem 1rem;
   border-radius: 3px;
   color: #fff;
   background: #ccc;
   border: 1px solid #999;
-  &:hover {
-    background: #bbb;
+  @media (min-width: 576px) {
+    &:hover {
+      background: #bbb;
+    }
   }
 ` 
 
 export const IconButton = styled.button`
   display: inline-flex;
   font-size: 1.25em;
-  &:hover {
-    color: #999;
+  @media (min-width: 576px) {
+    &:hover {
+      color: #999;
+    }
   }
 `
--- app/javascript/components/modules/Content.js
+++ app/javascript/components/modules/Content.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Content = () => {
   return (
     <div className="content">
-      <span className="top">勉強時間を</span>
-      <br/>
-      <span className="bottom">可視化します。</span>
+      <div className="inner">
+        <span className="top">勉強時間を</span>
+        <br/>
+        <span className="bottom">可視化します。</span>
+      </div>
     </div>
   )
 }
