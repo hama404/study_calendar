5007ac129e570167d9a85e7aa1b8dbeaf368d7fc
hamasaki <mfhama9910@icloud.com>
2021-07-03 08:53:35 +0900

    implement api

---
+++ api/src/db/migrate/20210418170842_create_lists.rb
@@ -0,0 +1,13 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+      t.string :name, null: false
+      t.string :date, null: false
+      t.integer :time, null: false
+      t.string :color, null: false
+      t.boolean :completed, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end
